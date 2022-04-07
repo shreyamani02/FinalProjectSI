1bb64d9128fe7d0e8833e7751cb5e6f17f17670d
SHREYA MANI <shreyamani@0587397337.wireless.umich.net>
2022-04-07 11:25:00 -0400

    finished code outline

---
+++ finalproject.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import sys
+import os
+import matplotlib
+import sqlite3
+import unittest
+import csv
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup 
+
+def setUpDatabase(db_name):
+    pass
+
+def scrapeImdb(soup):
+    pass
+
+def spotifyApi():
+    pass
+
+def youtubeAPI():
+    pass
+
+def updateAPI (cur, conn, data):
+    pass
+
+def avg_length_album(name, cur, conn):
+    pass
+
+def most_music_videos(cur, conn, data):
+    pass
+
+def avg_rating(cur, conn, album):
+    pass
+
+def avg_rating_graph(cur, conn, data):
+    pass
+
+def avg_length_graph(cur, conn, data):
+    pass
+
+def total_length_graph(cur, conn, data):
+    pass
+
+def pie_chart_genre(cur, conn, data):
+    pass
+
+def ratings_vs_rollingstone(cur, conn, data):
+    pass
+
