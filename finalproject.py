cd6cb7408587001ea0cb3c964aa3a87b30f9d2a8
SHREYA MANI <shreyamani@0587400160.wireless.umich.net>
2022-04-14 00:46:25 -0400

    finished pulling from spotify api and wiki

--- finalproject.py
+++ finalproject.py
@@ -1,13 +1,26 @@
-import requests
 import json
 import sys
 import os
 import matplotlib
 import sqlite3
-import unittest
 import csv
 import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+import requests
+import unittest
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
+
+
+cid = '80e4d2a8c2734c8e882a74e6f2c3e9bd'
+secret = '9a43668b2bcc4b02a047683c2226defc'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+name = "{Taylor Swift}"
+result = sp.search(name)
+print(result['tracks']['items'][0]['artists'])
+
+
 
 #Phoebe, Shreya, Isabelle
 
@@ -15,11 +28,48 @@ from bs4 import BeautifulSoup
 def setUpDatabase(db_name):
     pass
 
-def scrapeImdb(soup):
-    pass
+def scrapeWiki(soup):
+    wiki_dict = {}
+    new_dict = {}
+    iterator = 1
+    key = ""
+    for i in soup.findAll('table', class_ = 'collapsible collapsed'):
+        for winsnoms in i.findAll('td'):
+            if iterator == 1:
+                key = winsnoms.text.strip()
+                wiki_dict[key] = {}
+                iterator = 2
+            elif iterator == 2:
+                new_dict['wins'] = winsnoms.text.strip()
+                iterator = 3
+            else:
+                new_dict['noms'] = winsnoms.text.strip()
+                wiki_dict[key] = new_dict
+                new_dict = {}
+                iterator = 1
+    return wiki_dict
 
 def spotifyApi():
-    pass
+    artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
+    sp_albums = sp.artist_albums(artist_uri, album_type='album')
+    artist_data_list = []
+    for i in range(len(sp_albums['items'])):
+        new_dict = {}
+        new_dict['Album Name'] = sp_albums['items'][i]['name']
+        new_dict['Album Url'] = sp_albums['items'][i]['uri']
+        tracks = sp.album_tracks(new_dict['Album Url'])
+        tracklist = []
+        for n in range(len(tracks)):
+            data_dict = {}
+            data_dict["Song Name"] = tracks['items'][n]['name']
+            data_dict["Song Track Number"] = tracks['items'][n]['track_number']
+            data_dict["Song URL"] = tracks['items'][n]['uri']
+            data_dict["Track ID"] = tracks['items'][n]['id']
+            tracklist.append(data_dict)
+        new_dict['Track Data'] = tracklist
+        artist_data_list.append(new_dict)
+    print(artist_data_list)
+
 
 def youtubeAPI():
     pass
@@ -50,4 +100,13 @@ def pie_chart_genre(cur, conn, data):
 
 def ratings_vs_rollingstone(cur, conn, data):
     pass
+
+def main():
+    url = "https://en.wikipedia.org/wiki/List_of_awards_and_nominations_received_by_Taylor_Swift"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    scrapeWiki(soup)
+    spotifyApi()
+
+main()
 
