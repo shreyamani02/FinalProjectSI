e1ed60c47217f4af71c6762c63803d75ddc5cd7b
Isabelle Fisher <isabellefisher@0587338502.wireless.umich.net>
2022-04-25 20:29:24 -0400

    yt is driving me crazy but i have a nice pie chart

--- finalproject.py
+++ finalproject.py
@@ -24,7 +24,7 @@ result = sp.search(name)
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 #Phoebe, Shreya, Isabelle
-#hello 
+
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -163,20 +163,22 @@ def youtubeAPI(cur, conn):
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = api_key)
     
-    playlist = {"Red (Taylor's Version)": "OLAK5uy_lhEyrFap1OvMSwsL3AoZdrvqlRdJvyx0M",
-                "Fearless (Taylor's Version)": "OLAK5uy_lUwH9j_s3ZEeayUSm5o93gtQVz0If_kd8",
-                "Evermore":"OLAK5uy_m-vSVOiVeY_z2lPgThmS6Nn0TJExXZtOg",
-                "Folklore":"OLAK5uy_nWgO-2lNMsx90439Yx0xTWCGIktUc74e8",
-                "Lover":"OLAK5uy_nHHWc9S0Nw7oLyLYBptkJ4DpkQeoL1Igw",
-                "Reputation": "OLAK5uy_kyYsExXByLh2281MMfi0QvZJF5epEUxbk",
-                "1989":"OLAK5uy_lglIKPOFCG5X9_Rf4Hxsmmh9GEeHL94Jo",
-                "Red (Delux Addition)": "OLAK5uy_mwwCV3ci_DoOhgq27DRqnrVG3QOR_S2hQ",
-                "Speak Now": "OLAK5uy_k_sq8Sp6KDtHZIxW6ovITiJhl6SIC-5gw",
-                "Fearless":"OLAK5uy_kymlVnEd_mmMQfc4GJJPTNOW-ipnOhsrY"
+    playlist = {"red (taylor's version)": "OLAK5uy_lhEyrFap1OvMSwsL3AoZdrvqlRdJvyx0M",
+                "fearless (taylor's version)": "OLAK5uy_lUwH9j_s3ZEeayUSm5o93gtQVz0If_kd8",
+                "evermore":"OLAK5uy_m-vSVOiVeY_z2lPgThmS6Nn0TJExXZtOg",
+                "folklore":"OLAK5uy_nWgO-2lNMsx90439Yx0xTWCGIktUc74e8",
+                "lover":"OLAK5uy_nHHWc9S0Nw7oLyLYBptkJ4DpkQeoL1Igw",
+                "reputation": "OLAK5uy_kyYsExXByLh2281MMfi0QvZJF5epEUxbk",
+                "1989 (deluxe edition)":"OLAK5uy_lglIKPOFCG5X9_Rf4Hxsmmh9GEeHL94Jo",
+                "speak now (deluxe edition)": "OLAK5uy_k_sq8Sp6KDtHZIxW6ovITiJhl6SIC-5gw",
+                "message in a bottle (fat max g remix) (taylor’s version)":"OLAK5uy_n--XSLkZ2oX24BzALwE5oCHaBlPvx_L-I",
+                "folklore: the long pond studio sessions (from the disney+ special) [deluxe edition]":"OLAK5uy_kBCcQO_p5kZzSJDsJ7wA5WSSQ_FUL4Xu0",
+                "taylor swift": "OLAK5uy_l320Kcg2IKwpIRR07SD-ZejNX0cxRF32c",
+                "speak now world tour live":"OLAK5uy_kWNYh2JP5uga3mbNeIxmgQOoW6cxPDzgw",
                 }
     
     album_videos = {}
-    
+    vid_id = 0
     for album in playlist.keys():
         request = youtube.playlistItems().list(
             part="contentDetails, snippet, id, status",
@@ -187,15 +189,21 @@ def youtubeAPI(cur, conn):
         song_list = []
         items = response["items"]
         for i in items:
+            yt_id = i["id"]
             sub_it = i["snippet"]
             title = sub_it["title"].lower()
             date = sub_it["publishedAt"][:10]
+            print(date)
             digdate = int(date[:4]+date[5:7]+date[-2:])
             song_list.append((title, digdate))
+            #resp = youtube.videos().
         album_videos[album] = song_list
     for album in album_videos:
+        cur.execute('SELECT id from Albums WHERE album_title = ?',([album]))
+        album_id = int((cur.fetchone()[0]))
         for i in album_videos[album]:
-            cur.execute("INSERT OR IGNORE INTO Music_Videos (title, date) VALUES (?,?)", (i[0],i[1]))
+            cur.execute("INSERT OR IGNORE INTO Music_Videos (title, date, id, album_id) VALUES (?,?,?,?)", (i[0],i[1],vid_id, album_id,))
+            vid_id += 1
             conn.commit()
 
 def avg_winsnoms_ratio(cur, conn):
@@ -236,7 +244,7 @@ def most_popular_album(cur, conn):
         pop_list = []
         for a in pop_tup_list:
             pop_list.append(a[0])
-        if len(pop_list) != 0:
+        if len(pop_list) <=2:
             avg = sum(pop_list)/len(pop_list)
             pop_dict[i[0]]= avg
         else:
@@ -255,6 +263,12 @@ def album_time(album, cur, conn):
     total_length = sum(song_len_list)
     return total_length
 
+def videos_per_album(album, cur, conn):
+    cur.execute("SELECT song_title FROM Songs JOIN Albums ON albums.id = Songs.album_id WHERE album_title = (?)", (album,))
+    song_list = cur.fetchall()
+    res = len(song_list)
+    return res
+    
 def write_calculations(data):
     pass
 
@@ -281,8 +295,16 @@ def pie_chart_album_lengths(cur, conn):
         time = album_time(i[0], cur, conn)
         percent = time/total * 100
         fractions[i[0]] = percent
-    labels = fractions.keys()
-    sizes = fractions.values()
+    clean_fractions = {}
+    other = 0
+    for i in fractions.keys():
+        if fractions[i]<=1:
+            other += fractions[i]
+        else:
+            clean_fractions[i]=fractions[i]
+    clean_fractions['features and singles']= other
+    labels = clean_fractions.keys()
+    sizes = clean_fractions.values()
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, autopct='%1.1f%%',
             shadow=True, startangle=90)
@@ -291,6 +313,7 @@ def pie_chart_album_lengths(cur, conn):
           title="Album",
           loc="center left",
           bbox_to_anchor=(1, 0, 0.5, 1))
+    plt.title("Fraction of Total Song Time on Each Album")
     plt.show()
     return(plt)
 
@@ -317,10 +340,14 @@ def main():
     pie_chart_album_lengths(cur, conn)
     """
 
-main()
-"""
-cur, conn = setUpDatabase('db_vol_5.db')
-cur.execute("DROP TABLE IF EXISTS Songs")
-cur.execute("DROP TABLE IF EXISTS Albums")
-cur.execute("DROP TABLE IF EXISTS Genres")
-"""
+cur, conn = setUpDatabase('db_vol_7.db')
+#youtubeAPI(cur, conn)
+print(most_popular_album(cur, conn))
+
+#cur, conn = setUpDatabase('db_vol_8.db')
+#cur.execute("DROP TABLE IF EXISTS Songs")
+#cur.execute("DROP TABLE IF EXISTS Albums")
+#cur.execute("DROP TABLE IF EXISTS Music_Videos")
+
+
+
